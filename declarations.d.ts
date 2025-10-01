28a1997848b4cf13a6e4c691efb8e6d1e2b2cf23
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2025-10-01 20:23:59 +0200

    global.d.ts

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,13 +1,3 @@
 import { ShopwareClass } from "@ShopwareSrc/core/shopware"
 
 declare const Shopware: ShopwareClass;
-
-declare module '*.twig' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.scss' {
-  const content: string;
-  export default content;
-}
